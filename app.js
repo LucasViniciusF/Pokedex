bcffa7dae161919e065202d116f4f0838a10d5be
Lucas Ferreira <lucasvinicius.ferreira@outlook.com>
2022-11-19 12:28:27 -0300

    Refatorado js, criados campos do card, efeito e background

--- app.js
+++ app.js
@@ -3,15 +3,37 @@ const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
     fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
-const generateHTML = pokemons => pokemons.reduce((accumulator, {name, id, types}) => {
+const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types, weight, abilities, height, stats }) => {
     const elementTypes = types.map(typeInfo => typeInfo.type.name)
+    const abilityTypes = abilities.map(abilityInfo => abilityInfo.ability.name)
+    const statsTypes = stats.map(statsInfo => statsInfo.stat.name)
+    const statsValues = stats.map(valuesInfo => valuesInfo.base_stat)
 
     accumulator += `
-        <li class="card ${elementTypes[0]}">
-            <img class="card-image" alt"${name}" src="https://img.pokemondb.net/sprites/home/normal/${name}.png">
-            <h2 class="card-title">${id}. ${name}</h2>
-            <p class="card-subtitle">${elementTypes.join(' | ')}</p>
-        </li>
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">>
+                <li class="card ${elementTypes[0]}">
+                    <h2 class="card-title">${id}. ${name}</h2>
+                    <p class="card-subtitle">${elementTypes.join(' | ')}</p>
+                    <img class="card-image" alt"${name}" src="https://img.pokemondb.net/sprites/home/normal/${name}.png">
+                </li>
+            </div>
+            <div class="flip-card-back">
+                <li class="card ${elementTypes[0]}">
+                    <h2 class="card-title">${id}. ${name}</h2>
+                    <p class="card-subtitle">Type: ${elementTypes[0]}</p>
+                    <p class="card-subtitle">Weight: ${weight} - Height: ${height}</p>
+                    <p class="card-subtitle">Abilities: ${abilityTypes.join(' | ')}</p>
+                    <p class="card-subtitle">Stats:</p>
+                    <div class="statsInfo"> 
+                        <p class="card-subtitle">${statsTypes.join(' <br> ')}: </p>
+                        <p class="card-subtitle">${statsValues.join(' <br> ')}</p>            
+                    </div>
+                </li>
+            </div>
+        </div>
+    </div>
     `
     return accumulator
 }, '')
